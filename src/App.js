4240d013ca652b79ce3da5f023132c15d82a932a
Serhii-Zvieriev <zacjok@gmail.com>
2021-10-02 17:36:25 +0300

    complite hw

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import { Component } from 'react';
 import shortid from 'shortid';
 
 import style from './App.module.css';
-import Form from './components/Form';
-import List from './components/List';
+
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
+import Filter from './components/Filter';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  // nameInputId = shortid.generate();
+  addContacts = ({ name, number }) => {
+    if (this.state.contacts.some(contact => contact.name === name)) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
 
-  addContacts = name => {
     const contact = {
       id: shortid.generate(),
       name,
+      number,
     };
 
     this.setState(({ contacts }) => ({
@@ -24,18 +36,44 @@ class App extends Component {
   };
 
   formSubmitHendler = inputData => {
-    this.addContacts(inputData.name);
+    this.addContacts(inputData);
+  };
+
+  filterHendler = ({ target }) => {
+    const { value } = target;
+
+    this.setState({ filter: value });
+  };
+
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLocaleLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  deleteContact = ({ target }) => {
+    const { id } = target;
+    this.setState(precState => ({
+      contacts: precState.contacts.filter(contact => contact.id !== id),
+    }));
   };
 
   render() {
-    const { contacts } = this.state;
+    const { filter } = this.state;
 
     return (
       <div className={style.container}>
         <h2>Phonebook</h2>
-        <Form onSubmit={this.formSubmitHendler} />
+        <ContactForm onSubmit={this.formSubmitHendler} />
         <h2>Contacts</h2>
-        <List contacts={contacts} />
+        <Filter filter={filter} onChange={this.filterHendler} />
+        <ContactList
+          contacts={this.filterContacts()}
+          onDelete={this.deleteContact}
+        />
       </div>
     );
   }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,11 @@
 import { Component } from 'react';
 
-import style from './Form.module.scss';
+import style from './ContactForm.module.scss';
 
-class Form extends Component {
+class ContactForm extends Component {
   state = {
     name: '',
+    number: '',
   };
 
   handleChange = ({ target }) => {
@@ -23,30 +24,49 @@ class Form extends Component {
   resetForm = () => {
     this.setState({
       name: '',
+      number: '',
     });
   };
 
   render() {
+    const { name, number } = this.state;
+
     return (
       <form onSubmit={this.handleSubmit} className={style.form}>
         <label className={style.label}>
           Name
           <input
             onChange={this.handleChange}
-            value={this.state.name}
+            value={name}
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
             className={style.input}
+            required
           />
         </label>
 
-        <button type="submit">Add contact</button>
+        <label className={style.label}>
+          Number
+          <input
+            onChange={this.handleChange}
+            value={number}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            className={style.input}
+            required
+          />
+        </label>
+
+        <button type="submit" className={style.button}>
+          Add contact
+        </button>
       </form>
     );
   }
 }
 
-export default Form;
+export default ContactForm;
